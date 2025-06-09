c1b26311dec51e1b906bd71cc3e46b44f2697822
Dave Moore <dmoore@questcontrols.com>
2025-06-09 17:58:17 +0000

    Apply commit from ex-employee repo

--- umodbus/exceptions.py
+++ umodbus/exceptions.py
@@ -1,6 +1,74 @@
+from typing import Dict
+
+MODBUS_OP_CODES: Dict[int, str] = {
+    1: 'Read Coils (1)',
+    2: 'Read Discrete Inputs (2)',
+    3: 'Read Holding Registers (3)',
+    4: 'Read Input Registers (4)',
+    5: 'Write Single Coil (5)',
+    6: 'Write Single Register (6)',
+    7: 'Read Exception Status (7)',
+    8: 'Diagnostics (8)',
+    15: 'Write Multiple Coils (15)',
+    16: 'Write Multiple Registers (16)',
+    20: 'Read File Record (20)',
+    21: 'Write File Record (21)',
+    22: 'Mask Write Register (22)',
+    23: 'Read/Write Multiple Registers (23)',
+    24: 'Read FIFO (24)'
+}
+
+
 class ModbusError(Exception):
     """ Base class for all Modbus related exception. """
-    pass
+
+    def __init__(self, payload: bytes):
+        self._payload = payload
+
+    @property
+    def modbus_op_code(self) -> str:
+        """
+        Retrieves the Modbus operation code from the payload and returns a corresponding
+        description.
+
+        This property extracts the Modbus function code from the payload, adjusts it to account for
+        the error response offset (0x80), and then maps it to a human-readable description using
+        the MODBUS_OP_CODES dictionary.
+
+        Returns
+        -------
+        str
+            A string representing the Modbus function code. The possible return values are:
+            - 'Read Coils (1)'
+            - 'Read Discrete Inputs (2)'
+            - 'Read Holding Registers (3)'
+            - 'Read Input Registers (4)'
+            - 'Write Single Coil (5)'
+            - 'Write Single Register (6)'
+            - 'Read Exception Status (7)'
+            - 'Diagnostics (8)'
+            - 'Write Multiple Coils (15)'
+            - 'Write Multiple Registers (16)'
+            - 'Read File Record (20)'
+            - 'Write File Record (21)'
+            - 'Mask Write Register (22)'
+            - 'Read/Write Multiple Registers (23)'
+            - 'Read FIFO (24)'
+
+            If the function code is not recognized, it returns a string in the format
+            "Unknown Opcode: <opcode>".
+
+        Raises
+        ------
+        KeyError
+            If the Modbus function code is not found in the MODBUS_OP_CODES dictionary.
+        """
+        opcode = int(self._payload[0]) - int(0x80)
+
+        try:
+            return MODBUS_OP_CODES[opcode]
+        except KeyError:
+            return f"Unknown Opcode: {opcode}"
 
 
 class IllegalFunctionError(ModbusError):
